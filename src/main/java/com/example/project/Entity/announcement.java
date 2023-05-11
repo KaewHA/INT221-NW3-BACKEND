1945ef843b054d5d1e31e571c7d225b6259eb7d2
NatZuKix <nutthanon.natzuki@mail.kmutt.ac.th>
2023-05-11 12:09:03 +0700

    sprint 3

--- src/main/java/com/example/project/Entity/announcement.java
+++ src/main/java/com/example/project/Entity/announcement.java
@@ -14,7 +14,7 @@ import java.time.ZonedDateTime;
 @Setter
 @Getter
 @Entity
-@Table(name = "Announcemment")
+@Table(name = " announcements")
 public class  announcement  {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
